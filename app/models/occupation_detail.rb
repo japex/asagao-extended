b788876f86bf32c052b91cfbd46f83282507498c
Naoyuki Kumagai <kumagai@namaka>
2016-09-27 11:26:19 +0900

    Create model OccupationDetail

---
+++ app/models/occupation_detail.rb
@@ -0,0 +1,2 @@
+class OccupationDetail < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160923062303) do
+ActiveRecord::Schema.define(version: 20160927014503) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                       null: false
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 20160923062303) do
     t.datetime "updated_at",                      null: false
     t.string   "hashed_password"
     t.integer  "occupation_id"
+  end
+
+  create_table "occupation_details", force: :cascade do |t|
+    t.integer  "member_id"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "occupations", force: :cascade do |t|
